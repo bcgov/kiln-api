9f1508fc3b6ae8395f6386c14dfae71b186c4ad7
David Okulski <okulskiii@gmail.com>
2025-11-25 09:48:07 -0800

    Update port for deployment

--- server/index.ts
+++ server/index.ts
@@ -2,5 +2,5 @@ import './common/env';
 import Server from './common/server';
 import routes from './routes';
 
-const port = parseInt(process.env.PORT ?? '3000');
+const port = parseInt(process.env.PORT ?? '3001');
 export default new Server().router(routes).listen(port);
